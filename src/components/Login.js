0db67dd28a891ac7d4eebecc1556e2ca3b89b88a
HieuDo <dotrunghieu0903@gmail.com>
2024-08-04 22:55:10 -0400

    Updated Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,7 @@
 import { connect } from "react-redux";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 
 import logo from "../images/EmployeePolls.png"
 
@@ -7,11 +10,20 @@ const Login = ({loggedIn}) =>
     return (
         <div>
             <h1>Employee Polls</h1>
-            <img src={logo} alt="Logo"/>
+            <Image src={logo} alt="Logo" rounded />
             <h2>Log In</h2>
-            <form>
-
-            </form>
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>UserName</Form.Label>
+                    <Form.Control type="email" placeholder="Please input the username" />
+                </Form.Group>
+                <Form.Label htmlFor="inputPassword">Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    id="inputPassword"
+                />
+                <Button as="a" variant="primary">Submit</Button>
+            </Form>
         </div>
     )
 }
