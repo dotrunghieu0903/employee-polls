b9275c91bce1930efcb0425c475b2e588cbc7fae
HieuDo <dotrunghieu0903@gmail.com>
2024-08-05 03:23:10 -0400

    Updated Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,28 @@
+import * as React from "react";
 import { connect } from "react-redux";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import { Eye, EyeSlashFill } from "react-bootstrap-icons";
 
 import logo from "../images/EmployeePolls.png"
 
 const Login = ({loggedIn}) =>
 {
+    const [showPass, setShowPass] = React.useState(false);
+    const clickHandler = () => {
+        setShowPass((prev) => !prev);
+    };
     return (
         <div>
-            <h1>Employee Polls</h1>
-            <Image src={logo} alt="Logo" rounded />
-            <h2>Log In</h2>
+            <h2 style={{textAlign:"center"}}>Employee Polls</h2>
+            <Image src={logo} alt="Logo" width={100} style={{display:"block", marginLeft:"auto", marginRight:"auto",width:"25%"}} />
+            <h3 style={{textAlign:"center"}}>Log In</h3>
             <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>UserName</Form.Label>
+                <Form.Label style={{textAlign:"center"}}>UserName</Form.Label>
+                <Form.Group className="mb-3">
                     <Form.Control
                     id = "inputUsername"
                     data-testid="username"
@@ -25,9 +33,12 @@ const Login = ({loggedIn}) =>
                 <Form.Control
                     id="inputPassword"
                     data-testid="password"
-                    type="password"
+                    type={showPass ? "text" : "password"}
                     placeholder="Please input the password"
                 />
+                <InputGroup.Text onClick={clickHandler}>
+                    {showPass ? <EyeSlashFill /> : <Eye />}
+                </InputGroup.Text>
                 <Button as="a" variant="primary">Submit</Button>
             </Form>
         </div>
