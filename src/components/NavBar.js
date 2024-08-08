de8333c47a68b4a6bfcabb4283eb3aedb6becbbf
HieuDo <dotrunghieu0903@gmail.com>
2024-08-08 12:21:35 -0400

    Updated logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from "react-router-dom";
 import { Image, Nav, Navbar } from "react-bootstrap";
 
 import Avatar from "../images/Avatar.png"
+import { logout } from '../reducers/authorizationReducer';
 
 const NavBar = ({setNavbarHeight}) => {
     const authorization = useSelector(state => state.authorization);
@@ -11,6 +12,8 @@ const NavBar = ({setNavbarHeight}) => {
     const isActivePath = (path) => window.location.pathname === path;
 
     const navbarRef = useRef(null);
+    const dispatch = useDispatch();
+
     useEffect(() => {
         const updateNavbarHeight = () => {
             if(navbarRef.current) {
@@ -28,13 +31,17 @@ const NavBar = ({setNavbarHeight}) => {
 
 
     console.log(`state from NavBar ${JSON.stringify(authorization)}`)
+
+    const handleLogout = () => {
+        dispatch(logout());
+    };
+
     if (!authorization.isAuthenticated) {
         return <div>Please log in to view your profile. <Link to="/login">Login</Link></div>;
     }
 
     return (
         <Navbar ref={navbarRef} className="container" fixed="top" bg="dark" variant="dark" expand="lg">
-            <Navbar.Collapse>
             <Nav>
                 <Nav.Link className={isActivePath("/") ? "active" : ""} onClick={() => navigate("/")}>
                 Home
@@ -46,8 +53,10 @@ const NavBar = ({setNavbarHeight}) => {
                 New
                 </Nav.Link>
             </Nav>
+            <Navbar.Collapse className="d-flex justify-content-end">
                 <Image src={authorization.user.avatarURL ?? Avatar} alt="avatar" style={{width:"50px", height: "50px", marginRight: "10px"}} roundedCircle />
-                <p style={{color: "white"}}>{authorization.user.name}</p>
+                <Navbar.Text style={{color: "white"}}>{authorization.user.name}</Navbar.Text>
+                <Navbar.Text style={{color: "white", marginLeft: "10px", marginRight: "10px", cursor: "pointer"}} onClick={handleLogout}>Logout</Navbar.Text>
             </Navbar.Collapse> 
         </Navbar>
     )
