20bc12178eb3286715ab82196ce7a2a2d6c72397
HieuDo <dotrunghieu0903@gmail.com>
2024-08-08 12:34:38 -0400

    Updated the PageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,6 +1,11 @@
+import { Navbar } from "react-bootstrap";
+
 const PageNotFound = () => {
     return(
-        <h1>Page is not available</h1>
+        <div>
+            <h1>Page is not available</h1>
+            <Navbar.Brand style={{cursor: "pointer", color: "blue"}} href='/'>Go to HomePage</Navbar.Brand>
+        </div>
     )
 }
 
